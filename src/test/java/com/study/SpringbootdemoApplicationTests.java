6aba8011599ddbfbdd6be97d5377086601969874
chaxiaoniu <1935003650@qq.com>
2018-07-12 23:18:51 +0800

    1、@ConfigurationProperties：告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定。 2、导入配置文件处理器：spring-boot-configuration-processor

--- src/test/java/com/study/SpringbootdemoApplicationTests.java
+++ src/test/java/com/study/SpringbootdemoApplicationTests.java
@@ -1,16 +1,27 @@
 package com.study;
 
+import com.study.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+/**
+ * SpringBoot 单元测试类：
+ *
+ * 可以在测试期间可以很方便的类似编码一样进行自动注入等容器的功能
+ */
+@RunWith(SpringRunner.class)//② 单元测试使用SpringRunner驱动器来跑，而不是原来的JUnit
+@SpringBootTest//① 标识这是SpringBoot的单元测试
 public class SpringbootdemoApplicationTests {
+
+	@Autowired
+	Person person;
 
 	@Test
 	public void contextLoads() {
+		System.out.println(person);
 	}
 
 }
